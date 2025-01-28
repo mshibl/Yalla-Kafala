17475f4c83350a9e30f22abeabb3c3da0750074e
mohamedar97 <mohamedraafat060@gmail.com>
2025-01-28 17:22:40 +0200

    Disabled ssr on quiz page

--- src/app/(games)/games/quiz/page.tsx
+++ src/app/(games)/games/quiz/page.tsx
@@ -1,8 +1,10 @@
 import Quiz from "@/src/components/Games/Quiz";
 import { fetchQuizQuestions } from "@/src/utils/fetch-quiz";
 import { Box, Typography, Button } from "@mui/material";
+import { cookies } from "next/headers";
 
 export default async function PersonalityQuizPage() {
+  const cookieStore = await cookies(); // Getting cookies to disable cache
   try {
     const questions = await fetchQuizQuestions();
     return <Quiz questions={questions} />;
