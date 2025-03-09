d59ad40b8b44fedea758e0d0cf375d5cd266f383
Mohamed Shibl <mohamed.shibl@yallakafala.org>
2025-03-08 16:12:48 -0800

    whatsapp working branch with twilio

--- src/app/api/whatsapp-webhook/route.ts
+++ src/app/api/whatsapp-webhook/route.ts
@@ -1,4 +1,14 @@
-import { getMessagesByChatId } from "@/src/db";
+import {
+  createChat,
+  createMessage,
+  createUser,
+  getChatByUserId,
+  getMessageByWhatsappMessageSid,
+  getMessagesByChatId,
+  // getUserByPhoneNumber,
+  getUserByPhoneNumber,
+  getUserByWhatsappWaId,
+} from "@/src/db";
 import { NextResponse } from "next/server";
 
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
@@ -22,54 +32,227 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
+    const buf = await request.arrayBuffer();
+    const text = Buffer.from(buf).toString("utf-8");
+    const params = new URLSearchParams(text);
 
-    // Extract the message details
-    const entry = body.entry[0];
-    const changes = entry.changes[0];
-    const value = changes.value;
-    console.log("value", JSON.stringify(value, null, 2));
+    // Read the params
+    const body = params.get("Body");
+    const from = params.get("From");
+    const profileName = params.get("ProfileName");
+    const waId = params.get("WaId");
+    const smsMessageSid = params.get("SmsMessageSid");
+    const numMedia = params.get("NumMedia");
+    const messageType = params.get("MessageType");
+    const whatsappMessageType = params.get("MessageType");
+    const numSegments = params.get("NumSegments");
+    const referralNumMedia = params.get("ReferralNumMedia");
+    const apiVersion = params.get("ApiVersion");
+    // const mediaUrls = params.get("MediaUrls");
 
-    // Only process messages
-    if (value.messages && value.messages.length > 0) {
-      const messageContent = value.messages[0];
-      const name = value.contacts[0].profile.name;
-      const chatId = value.contacts[0].wa_id;
-      const from = messageContent.from;
-      const messageBody = messageContent.text.body;
+    // log all params
+    console.debug("[whatsapp-webhook] params", {
+      body,
+      from,
+      profileName,
+      waId,
+      smsMessageSid,
+      numMedia,
+      messageType,
+      whatsappMessageType,
+      numSegments,
+      referralNumMedia,
+      apiVersion,
+    });
 
-      // Fetch old messages in the conversation
-      const oldMessages = await getMessagesByChatId({ chatId });
-
-      const fullConversation = [
-        ...oldMessages,
-        { role: "user", content: messageBody },
-      ];
-
-      // send a request to the chat endpoint
-      const messageResponse = await fetch(
-        "https://www.yallakafala.org/api/chat",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            id: chatId,
-            messages: fullConversation,
-            authorName: name,
-            authorMobile: from,
-            blockingResponse: true,
-          }),
-        }
+    if (!waId) {
+      console.error(
+        "[whatsapp-webhook] No phone number found in webhook payload"
       );
-      const data = await messageResponse.json();
-      // Send response back
-      await sendWhatsAppMessage(from, data.text);
+      return new NextResponse(null, {
+        status: 200, // Send a 200 response to WhatsApp to acknowledge the message and avoid retries
+      });
     }
 
-    return new NextResponse("OK", { status: 200 });
+    if (!smsMessageSid) {
+      console.error(
+        "[whatsapp-webhook] No message ID found in webhook payload"
+      );
+      return new NextResponse(null, {
+        status: 200, // Send a 200 response to WhatsApp to acknowledge the message and avoid retries
+      });
+    }
+
+    // 1. find the user or create a new one
+    let user = await getUserByWhatsappWaId({ whatsappWaId: waId });
+
+    if (!user) {
+      user = await createUser({
+        phone: from?.split(":")[1], // "from" looks like: whatsapp:+14151234567, so we only want the +14151234567
+        name: profileName,
+        email: "",
+        signupMethod: "whatsapp",
+        whatsappProfileName: profileName,
+        whatsappWaId: waId,
+      });
+    }
+
+    // 2. find the chat or create a new one
+    let chat = await getChatByUserId({ userId: user.id });
+
+    if (!chat) {
+      chat = await createChat({ id: user.id, userId: user.id });
+    }
+
+    // 3. store the message
+    // Check idempotency
+    const existingMessage = await getMessageByWhatsappMessageSid({
+      whatsappMessageSid: smsMessageSid,
+    });
+
+    if (existingMessage) {
+      console.error(
+        "[whatsapp-webhook] Message already exists",
+        existingMessage
+      );
+      return new NextResponse(null, { status: 200 });
+    }
+
+    console.log("createMessage", {
+      chatId: chat.id,
+      role: "user",
+      content: body ?? "<Empty Message>",
+      authorName: profileName,
+      authorMobile: from,
+      whatsappMessageSid: smsMessageSid,
+      whatsappAccountSid: apiVersion,
+      whatsappMessagingServiceSid: PHONE_NUMBER_ID,
+      whatsappMessageType: whatsappMessageType,
+      whatsappNumMedia: numMedia,
+      whatsappNumSegments: numSegments,
+      whatsappReferralNumMedia: referralNumMedia,
+      whatsappApiVersion: apiVersion,
+    });
+
+    const message = await createMessage({
+      chatId: chat.id,
+      role: "user",
+      content: body ?? "<Empty Message>",
+      authorName: profileName,
+      authorMobile: from,
+      whatsappMessageSid: smsMessageSid,
+      whatsappAccountSid: apiVersion,
+      whatsappMessagingServiceSid: PHONE_NUMBER_ID,
+      whatsappMessageType: whatsappMessageType,
+      whatsappNumMedia: numMedia,
+      whatsappNumSegments: numSegments,
+      whatsappReferralNumMedia: referralNumMedia,
+      whatsappApiVersion: apiVersion,
+    });
+
+    console.log("message", message);
+
+    // check if the message is not a text message
+    // we only support text messages for now
+    if (messageType !== "text") {
+      console.error(
+        "[whatsapp-webhook] Message is not a text message",
+        message
+      );
+      return new NextResponse(
+        "Thanks for reaching out! 😊 At the moment, we only support text messages, but feel free to send us your questions here, and we’ll be happy to help!",
+        {
+          status: 200,
+        }
+      );
+    }
+
+    // 4. generate a response
+    const oldMessages = await getMessagesByChatId({ chatId: chat.id });
+
+    const fullConversation = [...oldMessages, message];
+
+    // send a request to the chat endpoint
+    const messageResponse = await fetch(
+      "https://www.yallakafala.org/api/chat",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          id: chat.id,
+          messages: fullConversation,
+          authorName: user.name,
+          authorMobile: from,
+          blockingResponse: true,
+        }),
+      }
+    );
+
+    const data = await messageResponse.json();
+
+    console.debug(
+      "[whatsapp-webhook] generated response data",
+      JSON.stringify(data, null, 2)
+    );
+
+    // 5. send a response
+    return new NextResponse(data.text, { status: 200 });
   } catch (error) {
-    console.error("Error processing webhook:", error);
-    return new NextResponse("Error", { status: 500 });
+    console.error("[whatsapp-webhook] Error processing webhook:", error);
+    return new NextResponse("Error Encountered", { status: 200 });
   }
+
+  // try {
+  //   // Read request from Twilio
+
+  //   // Extract the message details
+  //   const body = await request.json();
+
+  //   // Extract the message details
+  //   const entry = body.entry[0];
+  //   const changes = entry.changes[0];
+  //   const value = changes.value;
+  //   console.log("value", JSON.stringify(value, null, 2));
+
+  //   // Only process messages
+  //   if (value.messages && value.messages.length > 0) {
+  //     const messageContent = value.messages[0];
+  //     const name = value.contacts[0].profile.name;
+  //     const chatId = value.contacts[0].wa_id;
+  //     const from = messageContent.from;
+  //     const messageBody = messageContent.text.body;
+
+  //     // Fetch old messages in the conversation
+  //     const oldMessages = await getMessagesByChatId({ chatId });
+
+  //     const fullConversation = [
+  //       ...oldMessages,
+  //       { role: "user", content: messageBody },
+  //     ];
+
+  //     // send a request to the chat endpoint
+  //     const messageResponse = await fetch(
+  //       "https://www.yallakafala.org/api/chat",
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify({
+  //           id: chatId,
+  //           messages: fullConversation,
+  //           authorName: name,
+  //           authorMobile: from,
+  //           blockingResponse: true,
+  //         }),
+  //       }
+  //     );
+  //     const data = await messageResponse.json();
+  //     // Send response back
+  //     await sendWhatsAppMessage(from, data.text);
+  //   }
+
+  //   return new NextResponse("OK", { status: 200 });
+  // } catch (error) {
+  //   console.error("Error processing webhook:", error);
+  //   return new NextResponse("Error", { status: 500 });
+  // }
 }
 
 // Helper function to send WhatsApp messages
